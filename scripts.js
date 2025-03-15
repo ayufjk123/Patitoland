d634a1b6fb8e67542e9fb790b643b9f94274e708
ayufjk123 <ayufjk123@gmail.com>
2025-03-16 00:08:11 +0100

    update

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ hamburger.addEventListener('click', () => {
 
 // Close menu when clicking outside
 document.addEventListener('click', (e) => {
-    if (!e.target.closest('.nav-container')) {
+    if (!e.target.closest('.nav-container') && !e.target.closest('.nav-menu')) {
         hamburger.classList.remove('active');
         navMenu.classList.remove('active');
     }
